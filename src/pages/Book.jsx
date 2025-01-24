b72152ac2b95133d73eeee2519b41d28fd7b366a
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2025-01-24 11:37:28 +0100

    css e book.jsx

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,30 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function Book() {
+    const [data, setData] = useState([]);
+    const apiUrlBook = "http://localhost:3000/books";
+
+    useEffect(() => {
+        axios.get(apiUrlBook).then((res) => {
+            setData(res.data.results);
+        })
+            .catch((error) => {
+                console.log(error, "errore nella ricerca dei libri");
+            })
+    }, []);
     return (
-        <h1>sono book</h1>
+        <>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>
+                        <h2>{item.title}</h2>
+                        <h2>{item.author}</h2>
+                        <p>{item.abstract}</p>
+                        <img src={item.image} alt={item.title} />
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
