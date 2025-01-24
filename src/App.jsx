984836c24e137140862abd05157608a7046a6919
CarloDettori <dettoric07@gmail.com>
2025-01-24 10:21:37 +0100

    scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1>buonasera</h1>
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+export default function About() {
+    return (
+        <h1>sono about</h1>
+    )
+}
