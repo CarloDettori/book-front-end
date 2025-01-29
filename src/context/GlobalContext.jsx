05095348efb714e0ba856fc62fdaefad9ad7f5bf
Andrea050101 <agiancristiano90@gmail.com>
2025-01-29 20:14:03 +0100

    Fast Refresh bug fixed in global context

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,33 +1,28 @@
-import { createContext } from "react";
-import { useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-
-    const [booksList, setBooksList] = useState([])
+    const [booksList, setBooksList] = useState([]);
 
     function getBooksList() {
-
         axios
             .get("http://localhost:3000/books")
-            .then((res) => { setBooksList(res.data.items) })
-            .catch((error) => { console.log(console.log(error)) })
-            .finally("Data Fetch Completed");
-
+            .then((res) => setBooksList(res.data.items))
+            .catch((error) => console.log(error))
+            .finally(() => console.log("Data Fetch Completed"));
     }
 
-    useEffect(getBooksList, [])
+    useEffect(() => {
+        getBooksList();
+    }, []);
 
     return (
-
-        <GlobalContext.Provider value={{ booksList, setBooksList }} >
+        <GlobalContext.Provider value={{ booksList, setBooksList }}>
             {children}
         </GlobalContext.Provider>
+    );
+};
 
-    )
-
-}
-
-export { GlobalContext, GlobalProvider }
+export { GlobalContext, GlobalProvider };
