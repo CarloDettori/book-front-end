96eeff30f776cf86a2d4e3bc49b476df0a65233d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-01-30 10:09:33 +0100

    fixed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage() {
     return (
         <main className="d-flex justify-content-center align-items-center text-white">
             <div>
-                <h1>Benvenuto su Libraries</h1>
+                <h1>Vivi mille vite in una sola, la lettura è il primo passo</h1>
             </div>
         </main>
 
